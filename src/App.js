90c17dd033f219d8e8da743516e61c7c7e355141
fahim <skfahimd@gmail.com>
2021-07-27 21:08:24 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container m-2">
-        <Movies></Movies>
-      </main>
+      <div>
+        <NavBar />
+        <main className="container m-2">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect exact from="/" to="movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </div>
     );
   }
 }
